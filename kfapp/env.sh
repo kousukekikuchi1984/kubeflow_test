c12a09bdd96730558f5ea17d0687d5d3dc0ae0ed
kousukekikuchi1984 <kousukekikuchi1984@gmail.com>
2019-01-15 08:54:12 +0900

    kubeflow

---
+++ kfapp/env.sh
@@ -0,0 +1,7 @@
+PLATFORM=none
+KUBEFLOW_REPO=/Users/kosukekikuchi/study/kubeflow
+KUBEFLOW_VERSION=0.4.0
+KUBEFLOW_KS_DIR=/Users/kosukekikuchi/study/kubeflow/kfapp/ks_app
+KUBEFLOW_DOCKER_REGISTRY=
+K8S_NAMESPACE=kubeflow
+KUBEFLOW_PLATFORM=null
